62bc3eece9573fd603585a8bcc74c0c9745f158b
Prince Odia <princeodia@gmail.com>
2021-03-03 23:24:45 +0100

    Create a custom toast

--- ToastMe/app/src/main/java/com/khuosh/toastme/ToastActivity.java
+++ ToastMe/app/src/main/java/com/khuosh/toastme/ToastActivity.java
@@ -2,7 +2,9 @@ package com.khuosh.toastme;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +22,22 @@ public class ToastActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(this, R.string.hello_toast, Toast.LENGTH_SHORT);
         toast.show();
         // Position the toast object in the top-right corner
-        toast.setGravity(Gravity.TOP|Gravity.START|Gravity.END,
-                0, 130);
+        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.START|Gravity.END,
+                0, 0);
+    }
+
+    public void showCustomToast(View view) {
+        // Create a custom toast view
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.custom_toast, findViewById(R.id.custom_toast_container));
+
+        TextView textView = layout.findViewById(R.id.text);
+        textView.setText(getString(R.string.custom_toast_text));
+
+        Toast customToast = new Toast(this);
+        customToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        customToast.setDuration(Toast.LENGTH_SHORT);
+        customToast.setView(layout);
+        customToast.show();
     }
 }
